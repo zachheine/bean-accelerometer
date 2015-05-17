ec2ce05ce3302089e752720c92c561834079acbc
Zach Heineman <zach@heineman.info>
2015-05-17 00:34:39 -0700

    Update device

--- arduino/device.ino
+++ arduino/device.ino
@@ -1,32 +1,11 @@
-//
-// Copyright 2014, Evothings AB
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// LightBlue Bean - Basic
-//
-// Created October 28, 2014
-// Andreas Lundquist, Evothings AB
-//
 // This example shows how to use the Scratch data areas
 // to communicate with an external application.
 //
 // In this example the sketch fetches the LED values from
 // the scratch data area 1 and writes the current temperature
 // to the scratch data area 2.
-//
 
-String beanName = "LightBlueBean";
+String beanName = "Bean";
 const uint8_t ledScratch = 1;
 const uint8_t temperatureScratch = 2;
 
@@ -45,10 +24,17 @@ void loop() {
 	bool connected = Bean.getConnectionState();
 
 	if(connected) {
-		// Write current temperature to a scratch data area.
-		uint8_t temperatureBuffer[1];
-		temperatureBuffer[0] = Bean.getTemperature();
-		Bean.setScratchData(temperatureScratch, temperatureBuffer, 1);
+                AccelerationReading acceleration = Bean.getAcceleration();
+  
+  		// Write current temperature to a scratch data area.
+//		uint8_t temperatureBuffer[1];
+//		temperatureBuffer[0] = Bean.getTemperature();
+		uint8_t accelerationBuffer[3];
+                accelerationBuffer[0] = Bean.getAccelerationX();
+                accelerationBuffer[1] = Bean.getAccelerationY();
+                accelerationBuffer[2] = Bean.getAccelerationZ();
+
+		Bean.setScratchData(temperatureScratch, accelerationBuffer, 3);
 
 		// Update LEDs
 		ScratchData receivedData = Bean.readScratchData(ledScratch);
